3f2cb694c8bcbc02dc2b6a14ef33f2cbdbdc3218
AkashMete <73811249+AkashMete@users.noreply.github.com>
2022-03-14 16:53:00 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+import Join from "./components/Join/Join.js";
+import Chat from "./components/Chat/Chat.js";
+
+function App() {
+    return (
+        <div className="App">
+<Router>
+    <Routes>
+    <Route exact path="/" element={<Join />} />
+    <Route exact path="/chat" element={<Chat />} />
+    </Routes>
+</Router>
+        </div>
+    )
+}
+   
+
+
+
+export default App;
